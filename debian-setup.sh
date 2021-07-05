a57d98eb0180814c9d732f83e162e714436acde6
Omar2535 <omar2535@alumni.ubc.ca>
2021-07-04 19:15:54 -0700

    add vscode setup

--- debian-setup.sh
+++ debian-setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 # Install some packages that I use
-sudo apt-get install zsh curl vim tmux;
+sudo apt-get install zsh curl vim tmux terminator gnome-keyring;
 
 # Install chrome
 echo -e "\e[91mInstalling chrome";
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb -O ~/Downloads/google-chrome-stable-stable_current_amd64.deb;
-cd ~/Downloads && sudo apt install ./google-chrome-stable_current_amd64.deb;
+sudo apt install ~/.Downloads/google-chrome-stable_current_amd64.deb;
+
+# Install vscode
+echo -e "\e[91mInstalling visual studio code";
+wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64 -O ~/Downloads/vscode-stable_current_amd64.deb;
+sudo apt install ~/.Downloads/vscode-stable_current_amd64.deb;
+
