bcbc3ceac19e2dc0914749ac398cbb32f6044b50
Omar2535 <omar2535@alumni.ubc.ca>
2023-11-18 11:15:21 -0500

    add aad tools script

---
+++ tools/aad_tools.sh
@@ -0,0 +1,9 @@
+# Install powershell
+sudo apt-get update
+sudo apt-get install -y wget apt-transport-https software-properties-common
+source /etc/os-release
+wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+sudo apt-get update
+sudo apt-get install -y powershell
