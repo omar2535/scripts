9975ea4470861426a04915e6a8f0ddb8783e26e6
Omar2535 <omar2535@alumni.ubc.ca>
2023-11-18 00:12:24 -0500

    Add ubuntu setup script

---
+++ ubuntu/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install -y curl
+sudo apt-get install -y vim
+sudo apt-get install -y git
+sudo apt-get install -y tmux
+sudo apt-get install -y zsh
+sudo apt-get install -y python3-pip
+
+sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl
+curl https://pyenv.run | bash
