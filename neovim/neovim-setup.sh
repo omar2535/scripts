0f8285e5e2e41b7f2584f39325647970cacd70c5
Omar2535 <omar2535@alumni.ubc.ca>
2024-09-16 22:08:45 -0400

    Add neovim plugins

--- neovim/neovim-setup.sh
+++ neovim/neovim-setup.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+
 # Setup neovim
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ## Install neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
-sudo rm -rf /opt/nvim
-sudo tar -C /opt -xzf nvim-linux64.tar.gz
+# curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+# sudo rm -rf /opt/nvim
+# sudo tar -C /opt -xzf nvim-linux64.tar.gz
 
-
-
-
+# Link nvim config directory
+rm -rf ~/.config/nvim
+mkdir -p ~/.config/
+ln -s $SCRIPT_DIR/nvim ~/.config/
 
--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,29 +1,24 @@
--- Vim sets first
+-- Load dependencies
+require("config.lazy")
+require('onedark').load()
+
+-- Set vim config
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set number")
 
 
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
-end
-vim.opt.rtp:prepend(lazypath)
+-- Keybindings
+vim.keymap.set("n", "<C-P>", require('fzf-lua').files, { desc = "Fzf Files" })
+vim.keymap.set("n", "<C-l>", require('fzf-lua').live_grep, { desc = "Fzf live_grep" })
+vim.keymap.set("n", "<C-g>", require('fzf-lua').grep, { desc = "Fzf grep" })
 
-local plugins = {}
-local opts = {}
-
-require("lazy").setup(plugins, opts)
+-- For telescope
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
