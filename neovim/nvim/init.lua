1af801ffc8751d44a8e2da76cc327fb305403317
Omar2535 <omar2535@alumni.ubc.ca>
2024-09-29 10:30:59 -0400

    Set dual-boot nvim on vscode and lazy

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,45 +1,51 @@
--- Load dependencies
-require("config.lazy")
-
--- Set vim config
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set number")
-vim.cmd("set nowrap")
+-- Set vim config (these settings can be shared)
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.number = true
+vim.opt.wrap = false
 vim.g.mapleader = " "
 
--- Set up notify
-vim.notify = require('notify')
+-- Theme (you might need to adjust this if the colorscheme doesn't load properly in VSCode)
 
--- For fzf-lua
-vim.keymap.set("n", "<C-P>", require('fzf-lua').files, { desc = "Fzf Files" })
-vim.keymap.set("n", "<C-l>", require('fzf-lua').live_grep, { desc = "Fzf live_grep" })
-vim.keymap.set("n", "<C-g>", require('fzf-lua').grep, { desc = "Fzf grep" })
-vim.keymap.set("n", "<C-\\>", require('fzf-lua').buffers, { desc = "Fzf buffer" })
+if not vim.g.vscode then
+    -- Load dependencies
+    require("config.lazy")
+
+    -- Set up notify
+    vim.notify = require('notify')
+
+    -- For fzf-lua
+    vim.keymap.set("n", "<C-P>", require('fzf-lua').files, { desc = "Fzf Files" })
+    vim.keymap.set("n", "<C-l>", require('fzf-lua').live_grep, { desc = "Fzf live_grep" })
+    vim.keymap.set("n", "<C-g>", require('fzf-lua').grep, { desc = "Fzf grep" })
+    vim.keymap.set("n", "<C-\\>", require('fzf-lua').buffers, { desc = "Fzf buffer" })
+
+    -- For telescope
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+    -- For Neotree
+    vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
+    vim.keymap.set('n', '<leader>fr', ':Neotree reveal<CR>', {})
+
+    -- For LSP-config
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+    vim.keymap.set('n', 'gdd', vim.lsp.buf.definition, {})
+    vim.keymap.set('n', 'gtd', vim.lsp.buf.type_definition, {})
+    vim.keymap.set('n', 'gtr', vim.lsp.buf.references, {})
+    vim.keymap.set("n", "<leader>lrn", vim.lsp.buf.rename, {})
+    vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+    vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
 
 
--- For telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+	vim.cmd('colorscheme catppuccin-mocha')
+else
+    -- This is in VSCODE
+end
 
--- For Neotree
-vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
-vim.keymap.set('n', '<leader>fr', ':Neotree reveal<CR>', {})
-
--- For LSP-config
-vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-vim.keymap.set('n', 'gdd', vim.lsp.buf.definition, {})
-vim.keymap.set('n', 'gtd', vim.lsp.buf.type_definition, {})
-vim.keymap.set('n', 'gtr', vim.lsp.buf.references, {})
-vim.keymap.set("n", "<leader>lrn", vim.lsp.buf.rename, {})
-vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
-vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
-
--- Theme
-vim.cmd('colorscheme catppuccin-mocha')
