b8326de55f1e26717ddb182a343bd9a306a203d9
Omar2535 <omar2535@alumni.ubc.ca>
2024-10-15 18:52:59 -0400

    Add indent blankline plugin

---
+++ neovim/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,14 @@
+return {
+    'lukas-reineke/indent-blankline.nvim',
+    main = "ibl",
+    config = function()
+        require("ibl").setup {
+            exclude = {
+                filetypes = {
+                    "dashboard", "NvimTree", "alpha", "packer", "help", "lspinfo", "TelescopePrompt", "TelescopeResults", ""
+                },
+            }
+        }
+    end,
+}
+
