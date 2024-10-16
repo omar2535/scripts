96150af8a1866cde3cbbcfe350f4069ba8d2d8eb
Omar2535 <omar2535@alumni.ubc.ca>
2024-10-16 19:01:10 -0400

    Add trouble

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -7,7 +7,7 @@ vim.opt.number = true
 vim.opt.wrap = false
 vim.g.mapleader = " "
 vim.opt.number = true
-vim.opt.guicursor = "a:blinkon100"
+-- vim.opt.guicursor = "a:blinkon100"
 
 -- Make my config a bit more like normal text editors by allowing me to use the arrow keys to move around
 vim.api.nvim_set_keymap('i', '<C-H>', '<C-W>', { noremap = true, silent = true })
