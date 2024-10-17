8164f81af2c1ae1cf7c680bca4a41f5b1aeaa4ed
Omar2535 <omar2535@alumni.ubc.ca>
2024-10-17 15:58:20 -0400

    Add ruff to lsp config

--- neovim/nvim/lua/plugins/lsp-config.lua
+++ neovim/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require('mason-lspconfig').setup({
-                ensure_installed = { "lua_ls", "pyright" }
+                ensure_installed = { "lua_ls", "pyright", "ruff" }
             })
         end
     },
@@ -19,6 +19,7 @@ return {
             local lspconfig = require('lspconfig')
             lspconfig.lua_ls.setup({})
             lspconfig.pyright.setup({})
+            lspconfig.ruff.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
