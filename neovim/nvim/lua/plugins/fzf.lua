b00fe5df05e57b50c853620752135940a7b13548
Omar2535 <omar2535@alumni.ubc.ca>
2024-10-17 16:58:26 -0400

    FZF send to quickfix with CTRL+Q

--- neovim/nvim/lua/plugins/fzf.lua
+++ neovim/nvim/lua/plugins/fzf.lua
@@ -4,6 +4,12 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     -- calling `setup` is optional for customization
-    require("fzf-lua").setup({})
+    require("fzf-lua").setup({
+        keymap = {
+            fzf = {
+                ["ctrl-q"] = "select-all+accept"
+            },
+        }
+    })
   end
 }
