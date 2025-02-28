8ccf9598b5ba113fd59622e631492b0accda232b
Omar2535 <omar2535@alumni.ubc.ca>
2025-02-27 19:45:33 -0500

    Add more go configuration for treesitter

--- neovim/nvim/lua/plugins/lsp-config.lua
+++ neovim/nvim/lua/plugins/lsp-config.lua
@@ -20,6 +20,7 @@ return {
             lspconfig.lua_ls.setup({})
             lspconfig.pyright.setup({})
             lspconfig.ruff.setup({})
+            lspconfig.gopls.setup({})
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
